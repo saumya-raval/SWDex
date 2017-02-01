a8cbb0a9f30905a94d30d4ce6c7dcbff9928a938
Aashal New <saumyaraval@saumyas-mac.local>
2017-02-01 11:27:44 -0800

    Initial Commit

---
+++ ForceDex/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ForceDex
+//
+//  Created by Saumya Raval on 2/1/17.
+//  Copyright © 2017 PlayAgain. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
